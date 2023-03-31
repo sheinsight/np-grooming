16c2516de713516e3ba3111a8139d3fe350baac0
ityuany <519495771@qq.com>
2023-03-31 22:42:04 +0800

    review

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
 // write your code
 
-import { $ } from "execa";
 import { cac } from "cac";
-import enquirer from "enquirer";
-import { ReleaseType } from "semver";
-import { readPackageUp } from "read-pkg-up";
-import { writePackageSync } from "write-pkg";
-import { VersionPromptAnswer, getChoices } from "./utils.js";
-
-const normalizedReadResult = await readPackageUp();
-
-if (!normalizedReadResult) {
-  console.error("No package.json found");
-  process.exit(1);
-}
+import { loadPackageJson } from "./utils.js";
+import { bumpAction } from "./bump-action.js";
 
 const cli = cac("@shined/np-grooming");
 
@@ -21,24 +10,6 @@ cli
   .command("[cwd]", " bump version ")
   .alias("bump")
   .option("--prefix <prefix>", "Prefix for the version", { default: "v" })
-  .action(async (_, opt) => {
-    const { path, packageJson } = normalizedReadResult;
-    const choices = await getChoices(packageJson.version);
-    const res = await enquirer.prompt<VersionPromptAnswer>({
-      type: "autocomplete",
-      name: "version",
-      message: `Publish a new version of ${packageJson.name} (${packageJson.version})`,
-      choices,
-      result: (selected: ReleaseType) =>
-        choices.find((x) => x.name === selected)?.value!,
-    });
+  .action(bumpAction);
 
-    const tagV = `${opt.prefix}${res.version}`;
-    await $`git tag ${tagV}`;
-    if (!res.version.includes("snapshot")) {
-      packageJson.version = res.version;
-      writePackageSync(path, packageJson);
-    }
-  });
-
-cli.version(normalizedReadResult.packageJson.version).help().parse();
+cli.help().parse();
